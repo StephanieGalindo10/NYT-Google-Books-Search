c2b70a87bccc84e49fee3f5e9efd2ec613fa44f3
stephaniegalindo10 <stephanimatedworld@gmail.com>
2019-01-27 20:04:57 -0800

    change booklist

---
+++ my-app/src/componets/booklist.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Book from "./book";
+
+const BookList = props => {
+  return (
+    <div className="row bookList">
+      <ol>
+        {props.books.map((book, index) => (
+          <div key={index}>
+            <Book
+              isbn={book.isbns[0].isbn10}
+              author={book.book_details[0].author}
+              bookTitle={book.book_details[0].title}
+              publisher={book.book_details[0].publisher}
+              description={book.book_details[0].description}
+              rank={book.rank}
+              weeksOnList={book.weeks_on_list}
+			 rowNumber = {i}
+			highlight = {highlight}
+					
+            />
+          </div>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default BookList;
